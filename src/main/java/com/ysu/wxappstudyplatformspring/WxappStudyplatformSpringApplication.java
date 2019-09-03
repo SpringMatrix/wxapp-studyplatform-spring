9560789bacbd235ee48cadf72026db4a2068194b
Populus-euphratica <18832926135@163.com>
2019-09-03 11:00:51 +0800

    first commit

---
+++ src/main/java/com/ysu/wxappstudyplatformspring/WxappStudyplatformSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ysu.wxappstudyplatformspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxappStudyplatformSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxappStudyplatformSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ysu/wxappstudyplatformspring/WxappStudyplatformSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ysu.wxappstudyplatformspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WxappStudyplatformSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
