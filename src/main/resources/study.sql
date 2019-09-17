21d74e4ac067d46fa2b6eee262af763de0cf1612
RankOfMatrix <monoglo37@gmail.com>
2019-09-17 08:58:58 +0800

    数据库脚本更新

--- src/main/resources/study.sql
+++ src/main/resources/study.sql
@@ -1,148 +1,114 @@
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: study
--- ------------------------------------------------------
--- Server version	8.0.17
+-- --------------------------------------------------------
+-- 主机:                           localhost
+-- 服务器版本:                        10.3.14-MariaDB - mariadb.org binary distribution
+-- 服务器OS:                        Win64
+-- HeidiSQL 版本:                  10.2.0.5599
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `admin`
---
 
-DROP TABLE IF EXISTS `admin`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `admin` (
-  `admin_id` varchar(3) COLLATE utf8mb4_bin NOT NULL,
-  `name` varchar(15) COLLATE utf8mb4_bin NOT NULL,
-  `account` varchar(20) COLLATE utf8mb4_bin NOT NULL,
-  `password` varchar(16) COLLATE utf8mb4_bin NOT NULL,
+-- Dumping database structure for study
+CREATE DATABASE IF NOT EXISTS `study` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `study`;
+
+-- Dumping structure for table study.admin
+CREATE TABLE IF NOT EXISTS `admin` (
+  `admin_id` int(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '管理员id',
+  `name` varchar(15) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '管理员姓名',
+  `account` varchar(20) COLLATE utf8mb4_bin NOT NULL COMMENT '管理员账号',
+  `password` varchar(16) COLLATE utf8mb4_bin NOT NULL DEFAULT 'admin' COMMENT '管理员密码',
   PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=89 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
---
--- Table structure for table `bookmark`
---
+-- Data exporting was unselected.
 
-DROP TABLE IF EXISTS `bookmark`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `bookmark` (
-  `id` varchar(5) COLLATE utf8mb4_bin NOT NULL,
-  `unionid` varchar(30) COLLATE utf8mb4_bin NOT NULL,
-  `course_id` varchar(10) COLLATE utf8mb4_bin NOT NULL,
+-- Dumping structure for table study.bookmark
+CREATE TABLE IF NOT EXISTS `bookmark` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '收藏关系主键',
+  `unionid` varchar(30) COLLATE utf8mb4_bin NOT NULL COMMENT '用户id',
+  `course_id` int(11) unsigned NOT NULL DEFAULT 0 COMMENT '课程id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
---
--- Table structure for table `course`
---
+-- Data exporting was unselected.
 
-DROP TABLE IF EXISTS `course`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `course` (
-  `course_id` varchar(10) COLLATE utf8mb4_bin NOT NULL,
-  `unionid` varchar(30) COLLATE utf8mb4_bin NOT NULL,
-  `name` varchar(20) COLLATE utf8mb4_bin NOT NULL,
-  `introduction` varchar(200) COLLATE utf8mb4_bin NOT NULL,
-  `url` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+-- Dumping structure for table study.course
+CREATE TABLE IF NOT EXISTS `course` (
+  `course_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '课程id',
+  `unionid` varchar(30) COLLATE utf8mb4_bin NOT NULL COMMENT '上传者id',
+  `name` varchar(20) COLLATE utf8mb4_bin NOT NULL COMMENT '课程名称',
+  `image_url` varchar(500) COLLATE utf8mb4_bin NOT NULL COMMENT '缩略图链接',
+  `introduction` varchar(200) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '课程简介',
+  `url` varchar(500) COLLATE utf8mb4_bin NOT NULL COMMENT '视频链接',
   `tag1` varchar(10) COLLATE utf8mb4_bin NOT NULL,
   `tag2` varchar(10) COLLATE utf8mb4_bin DEFAULT NULL,
   `tag3` varchar(10) COLLATE utf8mb4_bin DEFAULT NULL,
-  `play_num` varchar(15) COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
-  `bookmark_num` varchar(15) COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
-  `visible` tinyint(4) NOT NULL DEFAULT '1',
+  `play_num` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '播放次数',
+  `bookmark_num` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '收藏次数',
+  `visible` tinyint(4) unsigned NOT NULL DEFAULT 0 COMMENT '是否可见',
   PRIMARY KEY (`course_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
---
--- Table structure for table `follow`
---
+-- Data exporting was unselected.
 
-DROP TABLE IF EXISTS `follow`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `follow` (
-  `id` int(11) NOT NULL,
-  `unionid1` varchar(30) COLLATE utf8mb4_bin NOT NULL,
-  `unionid2` varchar(30) COLLATE utf8mb4_bin NOT NULL,
+-- Dumping structure for table study.follow
+CREATE TABLE IF NOT EXISTS `follow` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '关注关系主键',
+  `unionid1` varchar(30) COLLATE utf8mb4_bin NOT NULL COMMENT '关注者id',
+  `unionid2` varchar(30) COLLATE utf8mb4_bin NOT NULL COMMENT '被关注者id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
---
--- Table structure for table `news`
---
+-- Data exporting was unselected.
 
-DROP TABLE IF EXISTS `news`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `news` (
-  `id` varchar(20) COLLATE utf8mb4_bin NOT NULL,
-  `unionid` varchar(30) COLLATE utf8mb4_bin NOT NULL,
-  `content` varchar(300) COLLATE utf8mb4_bin NOT NULL,
-  `time` datetime(1) NOT NULL,
-  `visible` tinyint(4) NOT NULL DEFAULT '1',
+-- Dumping structure for table study.news
+CREATE TABLE IF NOT EXISTS `news` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '动态id',
+  `unionid` varchar(30) COLLATE utf8mb4_bin NOT NULL COMMENT '用户id',
+  `name` varchar(30) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '用户昵称',
+  `avatar` varchar(130) COLLATE utf8mb4_bin NOT NULL COMMENT '用户头像',
+  `content` varchar(150) COLLATE utf8mb4_bin NOT NULL COMMENT '内容',
+  `time` datetime(1) NOT NULL COMMENT '发布时间',
+  `visible` tinyint(4) unsigned NOT NULL DEFAULT 0 COMMENT '是否可见',
+  PRIMARY KEY (`id`),
+  KEY `用户id关联` (`unionid`),
+  CONSTRAINT `用户id关联` FOREIGN KEY (`unionid`) REFERENCES `user` (`unionid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table study.note
+CREATE TABLE IF NOT EXISTS `note` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '笔记id',
+  `unionid` varchar(30) COLLATE utf8mb4_bin NOT NULL COMMENT '发布者id',
+  `course_id` int(11) unsigned NOT NULL COMMENT '课程id',
+  `content` varchar(300) COLLATE utf8mb4_bin NOT NULL COMMENT '笔记内容',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
---
--- Table structure for table `note`
---
+-- Data exporting was unselected.
 
-DROP TABLE IF EXISTS `note`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `note` (
-  `id` varchar(5) COLLATE utf8mb4_bin NOT NULL,
-  `unionid` varchar(30) COLLATE utf8mb4_bin NOT NULL,
-  `course_id` varchar(10) COLLATE utf8mb4_bin NOT NULL,
-  `content` varchar(300) COLLATE utf8mb4_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `unionid` varchar(30) COLLATE utf8mb4_bin NOT NULL,
-  `name` varchar(30) COLLATE utf8mb4_bin NOT NULL,
-  `sex` varchar(2) COLLATE utf8mb4_bin DEFAULT NULL,
-  `privacy` tinyint(4) NOT NULL DEFAULT '0',
-  `tag` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
-  `follow_num` varchar(15) COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
-  `punch_time` date DEFAULT NULL,
+-- Dumping structure for table study.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `unionid` varchar(30) COLLATE utf8mb4_bin NOT NULL COMMENT '用户id',
+  `name` varchar(30) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '用户昵称',
+  `sex` varchar(2) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '用户性别',
+  `privacy` tinyint(4) unsigned NOT NULL DEFAULT 0 COMMENT '隐私开关',
+  `tag` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '主标签',
+  `follow_num` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '粉丝数',
+  `punch_time` date DEFAULT NULL COMMENT '上次打卡时间',
+  `point` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '积分',
+  `avatar` varchar(130) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '头像url',
   PRIMARY KEY (`unionid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-09-04  9:27:08
